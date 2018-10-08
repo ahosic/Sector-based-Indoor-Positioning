9a6ea8aed5b61ef1a06e542f9cc94188bbed7bda
Almin Hosic <almin.hosic@gmail.com>
2018-10-08 21:06:55 +0200

    Fix Main class

--- src/main/kotlin/at/fhhgb/mc/Main.kt
+++ src/main/kotlin/at/fhhgb/mc/Main.kt
@@ -4,12 +4,9 @@ import at.fhhgb.mc.controller.MainController
 import at.fhhgb.mc.model.MainComponents
 import at.fhhgb.mc.view.MainFrame
 
-object Main {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val view = MainFrame()
-        val model = MainComponents()
-        val controller = MainController(view, model)
-        controller.start()
-    }
+fun main(args: Array<String>) {
+    val view = MainFrame()
+    val model = MainComponents()
+    val controller = MainController(view, model)
+    controller.start()
 }
