e8c257b42bc8169d61bff7b410fe7ebe232fb748
Almin Hosic <almin.hosic@gmail.com>
2018-11-07 19:18:40 +0100

    Add styles

--- src/main/kotlin/at/fhhgb/mc/component/sim/view/MapView.kt
+++ src/main/kotlin/at/fhhgb/mc/component/sim/view/MapView.kt
@@ -3,10 +3,11 @@ package at.fhhgb.mc.component.sim.view
 import javafx.embed.swing.SwingFXUtils
 import javafx.scene.image.ImageView
 import javafx.scene.layout.AnchorPane
+import javafx.scene.layout.BorderPane
 import javafx.scene.layout.Pane
 import java.awt.image.BufferedImage
 
-class MapView : AnchorPane() {
+class MapView : BorderPane() {
     private val imageView = ImageView()
     var mapImage: BufferedImage? = null
         set(value) {
@@ -15,11 +16,10 @@ class MapView : AnchorPane() {
         }
 
     init {
-        AnchorPane.setTopAnchor(imageView, 0.0)
-        AnchorPane.setBottomAnchor(imageView, 0.0)
-        AnchorPane.setLeftAnchor(imageView, 0.0)
-        AnchorPane.setRightAnchor(imageView, 0.0)
+        imageView.fitHeightProperty().bind(widthProperty())
+        imageView.fitHeightProperty().bind(heightProperty())
+        imageView.isPreserveRatio = true
 
-        children.add(imageView)
+        center = imageView
     }
 }
--- src/main/kotlin/at/fhhgb/mc/ui/SimulationApplication.kt
+++ src/main/kotlin/at/fhhgb/mc/ui/SimulationApplication.kt
@@ -3,26 +3,30 @@ package at.fhhgb.mc.ui
 import at.fhhgb.mc.component.sim.controller.NewSimulationController
 import at.fhhgb.mc.component.sim.model.SimulationModel
 import at.fhhgb.mc.component.sim.view.MapView
-import at.fhhgb.mc.controller.MainController
 import javafx.application.Application
 import javafx.geometry.Insets
 import javafx.geometry.Pos
 import javafx.scene.Scene
 import javafx.scene.control.Button
+import javafx.scene.layout.BorderPane
 import javafx.scene.layout.Pane
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
 import java.awt.Dimension
 import java.awt.image.BufferedImage
 import java.util.*
+import kotlin.math.roundToInt
 
 class SimulationApplication : Application(), Observer {
     var controller: NewSimulationController? = null
 
     private val mapView = MapView()
     private val playButton = Button("Play")
+    private val stylePath: String
 
     init {
+        stylePath = javaClass.classLoader.getResource("mainStyle.css").toExternalForm()
+
         val model = SimulationModel()
         controller = NewSimulationController(model)
         model.addObserver(this)
@@ -30,8 +34,10 @@ class SimulationApplication : Application(), Observer {
 
     override fun start(stage: Stage) {
         val layout = createLayout()
+
         with(stage) {
             scene = Scene(layout)
+            scene.stylesheets.add(stylePath)
             title = "Simulation"
             show()
         }
@@ -39,21 +45,30 @@ class SimulationApplication : Application(), Observer {
 
     private fun createLayout() : Pane {
         val layout = VBox()
+        layout.styleClass.add("mainLayout")
         layout.padding = Insets(8.0)
         layout.alignment = Pos.CENTER
 
+        initMapView()
+        initButtons()
+
+        layout.children.addAll(mapView, playButton)
+
+        return layout
+    }
+
+    private fun initMapView() {
+        mapView.styleClass.add("mapView")
         mapView.prefWidth = 800.0
         mapView.prefHeight = 500.0
         controller?.dimensions = Dimension(800, 500)
+    }
 
+    private fun initButtons() {
+        playButton.styleClass.add("playButton")
         playButton.setOnAction {
             controller?.onPlay(it)
         }
-
-        layout.children.add(mapView)
-        layout.children.add(playButton)
-
-        return layout
     }
 
     override fun update(o: Observable?, arg: Any?) {
